3c3f7381f1635c43e63efc149cefdb1fa4d51f5b
Pawel <pawel.kurek975@gmail.com>
2021-05-23 02:15:19 +0200

    slider final

--- src/Components/NextHours/NextHours.js
+++ src/Components/NextHours/NextHours.js
@@ -1,38 +1,82 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './NextHours.css';
 
 const NextHours = () => {
 
-    const testArray = [1,2,3,4,5,6,7,8];
-    let sliceStart = 0;
-    const sliceElementsCount = 5;
-    let sliceEnd = sliceStart+sliceElementsCount;
-    let sliceArray = testArray.slice(sliceStart,sliceEnd);
+    const hoursArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48];
 
-    const [state, setState] = useState(sliceArray)
-    console.log(state)
+    const hoursInSlider = [3,4,6,8,12];
+    const containerWidth = [1600,1200,800,600,400];
+    const maxSliderMove = [1500,1100,700,500,300]; 
+    const minSliderMove = 0;
+    // let sliderIndex = 0;
+
+    const [state, setState] = useState(hoursArray)
+    const [move,setMove] = useState(0);
+    const [sliderIndex, setSliderIndex] = useState(0)
     
-    const handleClick = () => {
-        sliceStart = sliceEnd;
-        sliceEnd = sliceStart+sliceElementsCount;
-        sliceArray = testArray.slice(sliceStart,sliceEnd);
-        console.log(sliceStart,sliceEnd,sliceArray)
-        setState(sliceArray)
+    const updateWindowWidth = () => {
+        const windowSize = window.innerWidth
+        if (windowSize <= 400) {
+            setSliderIndex(0)
+        } else if (windowSize <=800) {
+            setSliderIndex(1)
+        } else if (windowSize <=1400) {
+            setSliderIndex(2)
+        } else if (windowSize <=2000) {
+            setSliderIndex(3)
+        } else{
+            setSliderIndex(4)
+        }
     }
 
+    const handleMoveLeft = () => {
+        console.log(move + "left");
+        if(move===maxSliderMove[sliderIndex]) return
+        setMove(move+100);
+    }
+    
+    const handleMoveRight = () => {
+        console.log(move + "right");
+        if(move===minSliderMove) return
+        setMove(move-100);
+    }
+
+    window.addEventListener('resize', updateWindowWidth)
+
+    useEffect(() => {
+        updateWindowWidth()
+        
+    }, []);
+    
     const allHours = state.map(hour => (
-        <div style={{width:`calc(100%/${sliceElementsCount}`}} className="hours-weather__element">
+        <div 
+            style={{width:`calc(100%/${hoursInSlider[sliderIndex]}`}} className="hours-weather__element"
+        >
             <p>{hour}</p>
         </div>
     ))
 
     return(
-        <div className="hours-weather">
-            <button>{'<'}</button>
-                <div className="hours-weather__container">
+        <div className="container">
+                <button 
+                    className="hours-weather__button" 
+                    style={{zIndex:1}} 
+                    onClick={handleMoveRight}>
+                        {'<'}
+                </button>
+            <div className="hours-weather">
+                <div 
+                    style={{left:`-${move}%`, width:`${containerWidth[sliderIndex]}%`}} className="hours-weather__container"
+                >
                     {allHours}
                 </div>
-            <button onClick={handleClick}>{'>'}</button>
+            </div>
+                <button 
+                    className="hours-weather__button" 
+                    style={{zIndex:1}} onClick={handleMoveLeft}>
+                        {'>'}
+                </button>
         </div>
     )
 }
