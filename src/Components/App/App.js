5057d49d709f0bdb0d6af0d1e8615cb7d0fd298e
Pawel <pawel.kurek975@gmail.com>
2021-06-04 20:37:39 +0200

    responsive weather theme

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect } from 'react';
+import React, {useEffect, useRef } from 'react';
 import {useSelector,useDispatch} from 'react-redux';
 import './App.css';
 import axios from 'axios';
@@ -13,6 +13,7 @@ function App() {
 
   const isFilterSectionOpen = useSelector(state => state.isFilterSectionOpen);
   const dataLoading = useSelector(state => state.mainStateReducer.isDataLoading);
+  const mainStateReducer = useSelector(state => state.mainStateReducer);
   const dispatch = useDispatch()
 
   const formatTimestamp = (timestamp) => {
@@ -29,7 +30,8 @@ function App() {
   const exclude = "minutes";
   // const [newTime, setNewTime] = useState(null)
 
- 
+  const bgTest = useRef()
+
 
 useEffect(() => {
 
@@ -62,15 +64,33 @@ useEffect(() => {
           visibility: data.current.visibility,
         }));
         dispatch(setDataLoading(false)); 
+
+
+
+          const sunsetHour = Number((mainStateReducer.data[0].sunset).slice(0,2));
+          const sunsetMinutes = Number((mainStateReducer.data[0].sunset).slice(3,5));
+          let currentHour = new Date().getHours();
+          let currentMinutes = new Date().getMinutes();
+  
+          console.log(sunsetHour,sunsetMinutes, typeof sunsetHour, typeof currentHour);
+  
+           switch (mainStateReducer.data[0].weather) {
+            case 'clear sky':
+              if((currentHour < sunsetHour && currentMinutes < sunsetMinutes)) {
+                bgTest.current.style.background = weatherBackgroundColors['clearSkyN']
+              } else {
+                bgTest.current.style.background = weatherBackgroundColors['clearSkyD']
+              }
+              break;
+            default:
+              break;
+          }
+
       })
-      .catch(err => console.error(`Can't fetch the data from API - ${err}`))
-
+      .catch(err => console.error(`You have an error! - ${err}`))
+      
     }
-    
-
-
     setTimeout(fetchData,2000)
-
 
 }, [])
 
@@ -97,11 +117,33 @@ useEffect(() => {
 // ))
 
 
+const weatherBackgroundColors = {
+  clearSkyD: `linear-gradient(45deg,rgba(137, 219, 224,1),rgba(137, 219, 224,0))`,
+  clearSkyN: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  fewCloudsD: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  fewCloudsN: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  scatteredCloudsD: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  scatteredCloudsN: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  brokenCloudsD: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  brokenCloudsN: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  showerRainD: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  showerRainN: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  rainD: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  rainN: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  thunderstormD: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  thunderstormN: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  snowD: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  snowN: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  mistD: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+  mistN: `linear-gradient(45deg,rgba(44, 52, 62, 1),rgba(44, 52, 62, 0.85))`,
+
+}
+
 
 return (
-  <div className="app">
+  <div ref={bgTest} className="app" >
     <Nav/>
-    <div className="app__main-content">
+    <div  className="app__main-content">
       <div className="app__weather-icon" ></div>
       {!isFilterSectionOpen && <Quote/>}
       {!dataLoading && <Main/>}
