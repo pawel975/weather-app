24b8f4111ef389ca68e5e1a8b7761ff9e40bc61d
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-04-02 15:48:14 +0200

    delete unused code

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,7 +1,6 @@
 import './Nav.css';
 import React from 'react';
-// import {AiOutlineSearch} from 'react-icons/ai';
-// import {BiCurrentLocation} from 'react-icons/bi';
+
 
 const Nav = ({formatToDate}) => {
 
@@ -9,10 +8,6 @@ const Nav = ({formatToDate}) => {
 
     return(
         <div className="nav">
-            {/* <button className="nav__button-now">Now</button>
-            <input className="nav__input-search" type="text" style={{width:"50px", display: "none"}}/>
-            <AiOutlineSearch className="nav__magnifier"/>
-            <BiCurrentLocation className="nav__localization"/> */}
             <div className="nav__date">{`Hello, it's ${formatToDate(date, "dayOfTheWeek")}, ${formatToDate(date, 'day-month')}`}</div>
         </div>
     )
