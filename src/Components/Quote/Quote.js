35197048880164108fb276abd3c9ef6cebd2652b
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-04-09 10:17:00 +0200

    quote resizing main component issue fixed

--- src/Components/Quote/Quote.js
+++ src/Components/Quote/Quote.js
@@ -24,7 +24,7 @@ const Quote = () => {
             })    
         })
         
-        interval = setInterval(fetchQuoteData, 10000)
+        interval = setInterval(fetchQuoteData, 1000)
     }
     
     useEffect(() => {
